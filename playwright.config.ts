9751e6c6f93d56272e6a8ad862723ab59da9e0c8
fabianmolinab <molinafabian11b@gmail.com>
2024-09-04 20:33:01 -0500

    fix: add config playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
+  reporter: [['list'], ['html', { outputFolder: 'test-results/html' }]],
   webServer: {
     command: 'npm run dev',
     port: 3000,
